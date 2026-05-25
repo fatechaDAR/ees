8d63093c549e221176f7384d7b47229e3961854b
techa <fatechaanggarahmatulloh@gmail.com>
2026-05-25 13:43:55 +0000

    menghilangkan tombol notif di topbar layout dashboard admin dan panitia

--- resources/views/layouts/panitia.blade.php
+++ resources/views/layouts/panitia.blade.php
@@ -262,9 +262,6 @@
                 <h1 class="topbar__title">@yield('page_title', 'Dashboard')</h1>
 
                 <div class="topbar__actions">
-                    <button class="notification-btn" aria-label="Notifikasi">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
-                    </button>
 
                     <div class="user-profile">
                         <div class="user-profile__info">
